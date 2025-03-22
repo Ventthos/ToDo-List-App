7341d202a4c0a3b1939c91df6407641fe3802673
Ventthos <ferra04@live.com.mx>
2025-03-22 01:01:48 -0600

    Side navbar

--- app/src/main/java/com/ventthos/todo_list_app/MainActivity.kt
+++ app/src/main/java/com/ventthos/todo_list_app/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.ventthos.todo_list_app
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var navigationView: NavigationView
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var buttonPrueba: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.navigation_view)
+        buttonPrueba = findViewById(R.id.buttonPrueba)
+
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+
+            }
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
         }
+
+        buttonPrueba.setOnClickListener { drawerLayout.openDrawer(GravityCompat.START) }
     }
 }
