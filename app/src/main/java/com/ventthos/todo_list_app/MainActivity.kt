5a83aab3ce00ec73bf3ba7d7c39644bc6f76b1c2
Ventthos <ferra04@live.com.mx>
2025-03-22 22:19:21 -0600

    no c

--- app/src/main/java/com/ventthos/todo_list_app/MainActivity.kt
+++ app/src/main/java/com/ventthos/todo_list_app/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.ventthos.todo_list_app
 
+import android.os.Build
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Button
+import android.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -16,15 +19,25 @@ import com.google.android.material.navigation.NavigationView
 class MainActivity : AppCompatActivity() {
     lateinit var navigationView: NavigationView
     lateinit var drawerLayout: DrawerLayout
-    lateinit var buttonPrueba: Button
+    lateinit var drawerToggle: ActionBarDrawerToggle
+    lateinit var toolbar: androidx.appcompat.widget.Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+        window.statusBarColor = ContextCompat.getColor(this, R.color.mainColor)
+
+        // find views
         drawerLayout = findViewById(R.id.drawer_layout)
         navigationView = findViewById(R.id.navigation_view)
-        buttonPrueba = findViewById(R.id.buttonPrueba)
+        toolbar = findViewById(R.id.toolbar)
+
+        // Drawer configuration
+        drawerToggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.openDrawerDesc, R.string.closeDrawerDesc)
+        drawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
 
         navigationView.setNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
@@ -34,6 +47,6 @@ class MainActivity : AppCompatActivity() {
             true
         }
 
-        buttonPrueba.setOnClickListener { drawerLayout.openDrawer(GravityCompat.START) }
+
     }
 }
