6605274986e23b95f77994905ab737cceef9d842
Ventthos <ferra04@live.com.mx>
2025-05-20 06:59:43 -0600

    Instalando otra vez firebase pero bien

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     id("com.google.devtools.ksp")
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -41,6 +42,7 @@ dependencies {
     implementation("com.google.android.gms:play-services-auth:20.7.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation(libs.play.services.base)
+    implementation(libs.firebase.database)
     ksp("com.github.bumptech.glide:compiler:4.16.0")
     implementation("com.google.android.material:material:1.9.0")
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,11 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
+    alias(libs.plugins.google.gms.google.services) apply false
 }
 
 buildscript {
     dependencies {
-        classpath("com.google.gms:google-services:4.3.15") // Asegúrate de tener la última versión
+
     }
 }
